912c97b0259df30fcac683f5b7c94eec21e519c0
zoyron <sgrarora3@gmail.com>
2020-05-30 22:55:44 +0530

    typography first draft

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
 import "./styles.css";
 const App = () => {
   return (
     <div className="App">
+      <Typography variant="h1">Material ui app</Typography>
       <Button variant="contained" color="primary">
         Default contained button
       </Button>
