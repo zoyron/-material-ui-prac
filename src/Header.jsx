cde4678eb20827d4f5f61a7ffdc99ea83bc56cec
zoyron <sgrarora3@gmail.com>
2020-05-31 17:17:05 +0530

    added material icons

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
-
+import PersonRoundedIcon from "@material-ui/icons/PersonRounded";
 const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="caption">This is the header</Typography>
+        <PersonRoundedIcon />
+        <Typography variant="caption">Sagar Arora</Typography>
       </Toolbar>
     </AppBar>
   );
