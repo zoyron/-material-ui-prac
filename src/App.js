f9a10bed908ec640ccd99a62a21ab80992e01b9b
zoyron <sgrarora3@gmail.com>
2020-05-31 16:28:56 +0530

    basic layout done

--- src/App.js
+++ src/App.js
@@ -9,17 +9,7 @@ const App = () => {
       </Grid>
       <Grid item container>
         <Grid item xs={0} sm={2} />
-        <Grid item xs={8}>
-          <Typography variant="overline">
-            this is for the content. the content is this is for the content. the
-            content is this is for the content. the content is this is for the
-            content. the content is this is for the content. the content is this
-            is for the content. the content is this is for the content. the
-            content is this is for the content. the content is this is for the
-            content. the content is this is for the content. the content is this
-            is for the content. the content is
-          </Typography>
-        </Grid>
+        <Grid item xs={12} sm={8}></Grid>
         <Grid item xs={0} sm={2} />
       </Grid>
     </Grid>
