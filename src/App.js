2f183ed2174dc99dc5b1289ab31097e92f0e5169
zoyron <sgrarora3@gmail.com>
2020-06-01 11:54:28 +0530

    basic layout done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+const App = () => {
+  return (
+    <div>
+      <h1>material ui detailed study</h1>
+      <Typography variant="h4" color="textSecondary">
+        I like this new font
+      </Typography>
+      <p>obviously, the new font of vs code, not of the browser</p>
+    </div>
+  );
+};
+
+export default App;
